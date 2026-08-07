b4ea6ef7261e242a7617e6f8a270d41bf6d41ff1
jabes-christian <jabescristian08@gmail.com>
2026-08-06 22:37:41 -0300

    fix: correção no docker-compose do ms de email

--- email/src/main/java/com/ms/email/consumer/EmailConsumer.java
+++ email/src/main/java/com/ms/email/consumer/EmailConsumer.java
@@ -1,6 +1,5 @@
 package com.ms.email.consumer;
 
-import jakarta.mail.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -9,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class EmailConsumer {
 
     @RabbitListener(queues = "${broker.queue.email.name}")
-    public void listenEmail(@Payload String string) {
+    public void listenEmailQueue(@Payload String string) {
         System.out.println(string);
     }
 }
