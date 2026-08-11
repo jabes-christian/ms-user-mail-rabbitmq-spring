7b687e852690072b9d76cac87dea914296932376
jabes-christian <jabescristian08@gmail.com>
2026-08-10 21:31:00 -0300

    feat(producer): implementando o producer

--- user/src/main/java/com/ms/user/producer/UserProducer.java
+++ user/src/main/java/com/ms/user/producer/UserProducer.java
@@ -1,5 +1,30 @@
 package com.ms.user.producer;
 
+import com.ms.user.dto.EmailDto;
+import com.ms.user.entity.UserEntity;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserProducer {
 
+    final RabbitTemplate rabbitTemplate;
+
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value(value = "${broker.queue.email.name}")
+    private String routingKey;
+
+    public void publishMessageEmail(UserEntity userEntity) {
+        var emailDto = new EmailDto();
+        emailDto.setUserId(userEntity.getUserId());
+        emailDto.setEmailTo(userEntity.getEmail());
+        emailDto.setSubject("Cadastro realizado com sucesso!");
+        emailDto.setText(userEntity.getName() + ", seja bem vindo(a)! \nAgradecemos o seu cadastro, aproveite agora todos os recursos da nossa plataforma!");
+
+        rabbitTemplate.convertAndSend("", routingKey, emailDto);
+    }
 }
--- user/src/main/java/com/ms/user/service/UserService.java
+++ user/src/main/java/com/ms/user/service/UserService.java
@@ -1,19 +1,26 @@
 package com.ms.user.service;
 
 import com.ms.user.entity.UserEntity;
+import com.ms.user.producer.UserProducer;
 import com.ms.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    @Autowired
-    UserRepository userRepository;
+    final UserRepository userRepository;
+    final UserProducer userProducer;
+
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
+        this.userRepository = userRepository;
+        this.userProducer = userProducer;
+    }
 
     @Transactional
     public UserEntity save(UserEntity userEntity) {
-        return userRepository.save(userEntity);
+        userEntity =  userRepository.save(userEntity);
+        userProducer.publishMessageEmail(userEntity);
+        return userEntity;
     }
 }
